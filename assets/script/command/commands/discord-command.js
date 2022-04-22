13865768ef8fa8004f72076db41c4108223b7bbb
networkException <github@nwex.de>
2022-04-22 23:43:59 +0000

    Update build branch

---
+++ assets/script/command/commands/discord-command.js
@@ -0,0 +1,9 @@
+import { Command } from "../command.js";
+export class DiscordCommand extends Command {
+    constructor() {
+        super("discord", "Get the link to my Discord.");
+    }
+    async execute() {
+        this.sendFeedback(`<a href="https://discord.xpple.dev/" target="_blank">https://discord.xpple.dev/</a>`);
+    }
+}
